2c561d88e62a208209d2c637fae8af48554628a6
boscan805 <108883860+boscan805@users.noreply.github.com>
2022-07-11 13:13:27 -0400

    Errores
    Solucion

--- MiCrud/resources/views/alerts/request.blade.php
+++
@@ -1,7 +0,0 @@
-@if(count($errors) > 0)
-    <ul class="list-group">
-        @foreach($errors->all() as $error)
-            <li class="list-group-item list-group-item-danger">{!!$error!!}</li>
-        @endforeach
-    </ul>
-@endif
